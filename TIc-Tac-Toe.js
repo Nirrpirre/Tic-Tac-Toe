ab789b046ff558b04dc23851ced1e46c42b6d519
Noel <noel.forsstromeriksson@skola.taby.se>
2025-03-10 11:08:59 +0100

    First commit

---
+++ TIc-Tac-Toe.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+let players = [];
+let board = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+];
+let currentPlayer = "X";
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    if (players.length < 2) {
+        players.push(socket.id);
+        socket.emit("playerAssigned", players.length === 1 ? "X" : "O");
+
+        if (players.length === 2) {
+            io.emit("gameStart");
+        }
+    }else {
+        socket.emit("Full");
+        socket.disconnect();
+        return;
+    }
+
+    socket.on("makeMove", (data) => {
+        if (socket.id !== players[0] && currentPlayer === "X") return;
+        if (socket.id !== players[1] && currentPlayer === "O") return;
+
+        if (board[data.index] === ""){
+            board[data.index] = currentPlayer;
+            io.emit("gameOver", `${currentPlayer} wins!`);
+        }
+    })
+})
