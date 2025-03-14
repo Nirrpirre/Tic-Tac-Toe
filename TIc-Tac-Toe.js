65750b6318fd8360f1f4c3b7955f6d3bacaf2857
Noel <noel.forsstromeriksson@skola.taby.se>
2025-03-14 11:49:33 +0100

    Big Change

--- TIc-Tac-Toe.js
+++ TIc-Tac-Toe.js
@@ -1,65 +1,21 @@
-const express = require("express");
-const http = require("http");
-const { disconnect } = require("process");
-const socketIO = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
-
-let players = [];
-let board = [
-    "", "", "",
-    "", "", "",
-    "", "", ""
-];
-let currentPlayer = "X";
-
-app.use(express.static("public"));
-
-io.on("connection", (socket) => {
-    console.log("A user connected:", socket.id);
-
-    if (players.length < 2) {
-        players.push(socket.id);
-        socket.emit("playerAssigned", players.length === 1 ? "X" : "O");
-
-        if (players.length === 2) {
-            io.emit("gameStart");
-        }
-    }else {
-        socket.emit("Full");
-        socket.disconnect();
-        return;
-    }
-
-    socket.on("makeMove", (data) => {
-        if (socket.id !== players[0] && currentPlayer === "X") return;
-        if (socket.id !== players[1] && currentPlayer === "O") return;
-
-        if (board[data.index] === ""){
-            board[data.index] = currentPlayer;
-            io.emit("UpdateBoard", { board, player: currentPlayer });
-
-            if (checkWin(currentPlayer)) {
-                io.emit("gameOver", `${currentPlayer} wins!`)
-                resetGame();
-                return;
-            }
-
-            if(board.every(cell => cell !== "")) {
-                io.emit("gameOver", "Draw");
-                resetGame();
-                return;
-            }
-
-            currentPlayer = currentPlayer === "X" ? "O" : "X";
-        }
-    });
-
-    socket.on("disconnect", () => {
-        console.log("User disconnected:", socket.id);
-    })
+const express = require("express")
+const app = express()
 
 
+
+const path = require("path")
+const http = require("http")
+const {Server} = require("socket.io")
+
+const server = http.createServer(app)
+
+const io = new Server(server)
+app.use(express.static(path.resolve("")))
+
+app.get("/", (req,res)=>{
+    return res.sendFile("index.html")
+})
+
+server.listen(3000,()=>{
+    console.log("port connected to 3000")
 })
